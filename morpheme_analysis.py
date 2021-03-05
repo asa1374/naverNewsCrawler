25bad6c83881beba5335087f7d7c11fbabd85029
asa1374 <asa1374@gmail.com>
2021-03-05 09:23:07 +0900

    형태소분석 stop

--- morpheme_analysis.py
+++ morpheme_analysis.py
@@ -1,7 +1,12 @@
 # -*- coding:utf-8 -*-
 import psycopg2
-# str1 = 사업구분과 평가유형을 분류하여 업데이트 해야 할 첫번째 구분이 됨
-# res1 = 사업구분 및 평가유형의 결과를 받아옴
+from konlpy.tag import Okt
+import re
+from collections import Counter
+from wordcloud import WordCloud
+
+import nltk
+
 
 #db 연결
 database = psycopg2.connect(host='192.168.0.135', dbname='ICT_WEB_CRW', user='postgres', password='postgrespw',
@@ -9,10 +14,40 @@ database = psycopg2.connect(host='192.168.0.135', dbname='ICT_WEB_CRW', user='po
 cursor = database.cursor()
 
 # 본문내용 불러오기
-str1 = """select contents from news_data_sum_1990_1999 where "no" = 1 """
+str1 = """select contents from news_data_sum_1990_1999 where "no" = 3 """
 cursor.execute(str1)
-res1 = cursor.fetchall()
-print(res1)
+res1 = cursor.fetchone()
+
+print(res1[0])
+nobr = re.sub('<br/>', '', res1[0])
+
+okt = Okt()
+#단어만 추출
+#token_ko = okt.nouns(res1[0])
+#어절만 추출
+#okt.phrases(res1[0])
+
+token_ko = okt.nouns(res1[0])
+count = Counter(token_ko)
+
+
+noun_list = count.most_common(10000)
+print(noun_list)
+# for v in noun_list:
+#     print(v)
+    # res = v[0] + " (" + str(v[1]) + ")"
+
+
+
+
+#
+# wc = WordCloud(font_path='C:\\Windows\\Fonts\\08SeoulNamsanB_0.ttf', \
+#                background_color="white", \
+#                width=1000, \
+#                height=1000, \
+#                max_words=100, \
+#                max_font_size=300)
+
 
 
 # Commit the transaction
